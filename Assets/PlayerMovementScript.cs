2e7f0a008c94dbcdf114460a18ece7e33cccc7d1
Andru <buscu.andru@ichb.ro>
2021-03-30 18:02:58 +0300

    First commit

---
+++ Assets/PlayerMovementScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour {
+
+    public float moveSpeed;
+
+    private Vector3 moveDirection;
+
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(moveDirection) * moveSpeed * Time.deltaTime);
+    }
+}
